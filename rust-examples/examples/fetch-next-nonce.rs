f332158d48f1fe3c98bfff6e7baea7ded8ff6db8
Stephen Skeirik <stephen.skeirik@pi2.network>
2026-02-16 09:14:01 -0500

    add fetch-next-nonce example/snippet

---
+++ rust-examples/examples/fetch-next-nonce.rs
@@ -0,0 +1,13 @@
+#[tokio::main()]
+async fn main() -> anyhow::Result<()> {
+  // begin-snippet: fetch-next-nonce
+  use jsonrpsee::http_client::HttpClient;
+  use fast_rust_examples::fastset_types::get_key_pair;
+  use fast_rust_examples::client::ProxyRpcClient;
+  let (sender_pub_key, _sender_priv_key) = get_key_pair();
+  let client = HttpClient::builder().build("https://proxy.fastset.xyz")?;
+  let next_nonce = client.get_account_info(sender_pub_key, None, None, None).await?.next_nonce;
+  println!("Next nonce for account {sender_pub_key} is {next_nonce}");
+  // end-snippet
+  Ok(())
+}
